82dc57f26b8006169e7025bee6d3e093c6e6cadc
cristiii <cristian.vlad10@gmail.com>
2023-03-26 22:35:18 +0300

    added RouteLocator bean

--- src/main/java/com/example/msgateway/MsGatewayApplication.java
+++ src/main/java/com/example/msgateway/MsGatewayApplication.java
@@ -2,6 +2,11 @@ package com.example.msgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class MsGatewayApplication {
@@ -10,4 +15,24 @@ public class MsGatewayApplication {
         SpringApplication.run(MsGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/bank/accounts/**")
+                        .filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/bank/loans/**")
+                        .filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/bank/cards/**")
+                        .filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://CARDS"))
+                .build();
+    }
 }
